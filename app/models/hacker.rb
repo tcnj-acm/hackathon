0ddbb1f776ca03b4224b55f5417779994b02b2f4
Mozdzem1 <mozdzem1@tcnj.edu>
2020-12-30 13:36:43 -0500

    Bug Fixes For Address Info
    Fixes bugs and code errors regarding address info updates.

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -198,57 +198,58 @@ class Hacker < ApplicationRecord
   # Returns an array of acceptable state options
   def self.states
     [
-      Alabama
-      Alaska
-      Arizona
-      Arkansas
-      California
-      Colorado
-      Connecticut
-      Delaware
-      Florida
-      Georgia
-      Hawaii
-      Idaho
-      Illinois
-      Indiana
-      Iowa
-      Kansas
-      Kentucky
-      Louisiana
-      Maine
-      Maryland
-      Massachusetts
-      Michigan
-      Minnesota
-      Mississipi
-      Missouri
-      Montana
-      Nebraska
-      Nevada
-      New Hampshire
-      New Jersey
-      New Mexico
-      New York
-      North Carolina
-      North Dakota
-      Ohio
-      Oklahoma
-      Oregon
-      Pennsylvania
-      Rhode Island
-      South Carolina
-      South Dakota
-      Tennessee
-      Texas
-      Utah
-      Vermont
-      Virginia
-      Washington
-      West Virginia
-      Wisconsin
-      Wyoming
+      'Alabama',
+      'Alaska',
+      'Arizona',
+      'Arkansas',
+      'California',
+      'Colorado',
+      'Connecticut',
+      'Delaware',
+      'Florida',
+      'Georgia',
+      'Hawaii',
+      'Idaho',
+      'Illinois',
+      'Indiana',
+      'Iowa',
+      'Kansas',
+      'Kentucky',
+      'Louisiana',
+      'Maine',
+      'Maryland',
+      'Massachusetts',
+      'Michigan',
+      'Minnesota',
+      'Mississipi',
+      'Missouri',
+      'Montana',
+      'Nebraska',
+      'Nevada',
+      'New Hampshire',
+      'New Jersey',
+      'New Mexico',
+      'New York',
+      'North Carolina',
+      'North Dakota',
+      'Ohio',
+      'Oklahoma',
+      'Oregon',
+      'Pennsylvania',
+      'Rhode Island',
+      'South Carolina',
+      'South Dakota',
+      'Tennessee',
+      'Texas',
+      'Utah',
+      'Vermont',
+      'Virginia',
+      'Washington',
+      'West Virginia',
+      'Wisconsin',
+      'Wyoming'
       ]
+    end
 
   ##
   # Returns true iff the hackathon has not exceeded its capacity as defined by
